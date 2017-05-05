e297b17a4cb2954910f91fe2719833154b522e01
AHC <zxysegh@gmail.com>
2017-05-05 18:03:32 +0800

    Update UserController.java

--- src/main/java/org/aop/test002springmvc/action/UserController.java
+++ src/main/java/org/aop/test002springmvc/action/UserController.java
@@ -2,10 +2,16 @@ package org.aop.test002springmvc.action;
 
 import org.aop.test002springmvc.Model.User;
 import org.aop.test002springmvc.service.UserService;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Future;
 
 /**
  * Created by Admin on 2017/5/4.
@@ -14,17 +20,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/user")
 public class UserController {
 
+    private static final Log LOG = LogFactory.getLog(UserController.class);
+
     private UserService userService;
 
+    private ExecutorService executorService;
+
     @Autowired
-    public UserController(UserService userService) {
+    public UserController(UserService userService, ExecutorService executorService) {
         this.userService = userService;
+        this.executorService = executorService;
     }
 
     @RequestMapping("/add.do")
-    public String user(ModelMap modelMap) {
-        modelMap.addAttribute("username", "新用户");
+    public String user(final ModelMap modelMap) {
         userService.add("新用户");
-        return "user";
+        Future<String> future = executorService.submit(() -> {
+            modelMap.addAttribute("username", "新用户");
+            System.out.println("新用户");
+            return "user";
+        });
+        try {
+            LOG.info("--新用户--");
+            return future.get();
+        } catch (InterruptedException e) {
+            LOG.warn(e.getMessage());
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            LOG.warn(e.getMessage());
+            e.printStackTrace();
+        }
+
+        return "index";
     }
 }
